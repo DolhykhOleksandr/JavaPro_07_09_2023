b2ee44f8620d8074ef9d9b29615578656d25b472
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-09-09 19:41:32 +0300

    completed first homework

---
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework1/Car.java
@@ -0,0 +1,22 @@
+package ua.hillel.dolhykh.homeworks.homework1;
+
+
+public class Car {
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startFuelSystem() {
+
+    }
+
+    private void startCommand() {
+
+    }
+
+    private void startElectricity() {
+
+    }
+}
