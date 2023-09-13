cf184bb504d41cff02d2252b0389873fb704ce9e
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-09-13 23:15:48 +0300

    Completed homework Inheritance in Java by two variants

--- src/main/java/ua/hillel/dolhykh/homeworks/homework2/variant2/Animal.java
+++ src/main/java/ua/hillel/dolhykh/homeworks/homework2/variant2/Animal.java
@@ -5,7 +5,6 @@ public class Animal {
     public void run(int length) {
         System.out.println("Animal ran " + length + " m");
     }
-
     public void swim(int length) {
         System.out.println("Animal swam " + length + " m");
     }
