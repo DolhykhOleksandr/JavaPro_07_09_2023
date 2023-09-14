9251698248bd0a7fb81f04a2154690a9ff2a60b7
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-09-14 15:24:36 +0300

    minor changes2

--- src/main/java/ua/hillel/dolhykh/lessons/lesson1/Main.java
+++ src/main/java/ua/hillel/dolhykh/lessons/lesson1/Main.java
@@ -2,7 +2,8 @@ package ua.hillel.dolhykh.lessons.lesson1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");s
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
     }
 }
