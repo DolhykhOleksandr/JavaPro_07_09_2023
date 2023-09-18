ec4b846c07a30ab552c53c3721fa29873ba06290
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-09-18 21:54:07 +0300

    delete class

--- src/main/java/ua/hillel/dolhykh/homeworks/homework6/variant2/ArrayDataException.java
+++
@@ -1,8 +0,0 @@
-//package ua.hillel.dolhykh.homeworks.homework6.variant2;
-//
-//public class ArrayDataException extends RuntimeException {
-//
-//    public ArrayDataException(int line, int column) {
-//        super("This is not a number in a cell [" + line + "][" + column + "]");
-//    }
-//}
--- src/main/java/ua/hillel/dolhykh/homeworks/homework6/variant2/ArraySizeException.java
+++
@@ -1,8 +0,0 @@
-//package ua.hillel.dolhykh.homeworks.homework6.variant2;
-//
-//public class ArraySizeException extends RuntimeException {
-//
-//    public ArraySizeException() {
-//        super("Array has the wrong size");
-//    }
-//}
